a0fb54b8c97b31b95b7610c73bffdce8a6a239af
Maarten de Vries <maarten@de-vri.es>
2021-03-27 22:42:32 +0100

    Add initial drawing traits/types.

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,28 @@
 mod geometry;
 pub use geometry::*;
+
+pub trait Drawable {
+	fn draw(&self, surface: &mut Surface, position: Vector2);
+
+	fn set_max_width(&mut self, width: Option<Length>);
+
+	fn get_max_width(&self) -> Option<Length>;
+
+	fn size(&self) -> Vector2;
+
+	fn width(&self) -> Length {
+		self.size().x
+	}
+
+	fn height(&self) -> Length {
+		self.size().y
+	}
+}
+
+pub struct Context {
+	pango: pango::Context,
+}
+
+pub struct Surface {
+	cairo: cairo::Context,
+}
